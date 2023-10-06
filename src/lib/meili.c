e684d3f04d71e672e00da30cea1a3ea11efdc52c
shaofeng <wsf123@link.cuhk.edu.hk>
2023-10-07 00:02:41 +0800

    update

--- src/lib/meili.c
+++ src/lib/meili.c
@@ -8,37 +8,47 @@ volatile struct _meili_apis Meili;
 /* pkt_trans
 *   - Run a packet transformation operation specified by UCO.  
 */
+void pkt_trans(){};
 
 /* pkt_flt
 *   - Filter packets with the operation specified by UCO.
 */
+void pkt_flt(){};
 
 /* flow_ext
 *   - Construct flows from a stream based on UCO.
 */
-
+void flow_ext(){};
 
 /* flow_trans
 *   - Run a flow transformation operation specified by UCO.
 */
+void flow_trans(){};
 
 /* reg_sock
 *   - Register an established socket to Meili.
 */
+void reg_sock(){};
 
 /* epoll
 *   - Process an event on the socket with the operation specified by UCO.
 */
+void epoll(){};
 
 /* regex
 *   - The built-in Regular Expression API.   
 */
+void regex(){};
+
 /* AES
 *   - The built-in AES Encryption API.
 */
+void AES(){};
+
 /* compression
 *   - The built-in Compression API.
 */
+void compression(){};
 
 void register_meili_apis(){
     Meili.pkt_trans     = pkt_trans;
