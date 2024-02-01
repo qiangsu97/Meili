4c202021a2501c3da176a7b92abef28de988c5a6
shaofeng <wsf123@link.cuhk.edu.hk>
2024-02-01 17:37:47 +0800

    update

--- src/lib/regex/meili_regex.c
+++ src/lib/regex/meili_regex.c
@@ -6,32 +6,26 @@ int meili_regex_init(pl_conf *run_conf){
     
 	int ret;
 
-    printf("regex_dev_register()...\n");
 	/* Register corresponding regex device operation functions according to regex_dev_type in pl_conf */
 	ret = regex_dev_register(run_conf);
 	if (ret) {
 		MEILI_LOG_ERR("Regex dev registration error");
 		return -EINVAL;
 	}
-    printf("regex_dev_register() finished\n");
 
 
-    printf("regex_dev_compile_rules()...\n");
 	/* Compile rules if necessary or directly use compiled rules to program regex device */
 	ret = regex_dev_compile_rules(run_conf);
 	if (ret) {
 		MEILI_LOG_ERR("Regex dev rule compilation error");
 		return -EINVAL;
 	}
-    printf("regex_dev_compile_rules() finished\n");
 
-    printf("regex_dev_init()...\n");
 	/* Initalize regex device(program regex device, construct neccessary structures such as queues, operation buffers, etc.) */
 	ret = regex_dev_init(run_conf);
 	if (ret) {
 		MEILI_LOG_ERR("Failed initialising regex device");
 		return -EINVAL;
 	}
-    // printf("regex_dev_init() finished\n");
 	return 0;
 }
